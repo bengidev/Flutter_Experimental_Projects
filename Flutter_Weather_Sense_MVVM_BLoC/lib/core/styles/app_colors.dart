249a923c31efbdebd40e936128bca26cbc5141e5
syndicate017 <bambang.trd17@gmail.com>
2023-03-27 18:03:09 +0700

    Add colors for use throughout the project.

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/styles/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final primary = const Color(0xFF676DC9);
+  final secondary = const Color(0xFF484F9D);
+  final tertiary = const Color(0xFFF8CA56);
+  final neutral = const Color(0xFF929094);
+  final offWhite = const Color(0xFFF8ECE5);
+  final caption = const Color(0xFF7D7873);
+  final body = const Color(0xFF514F4D);
+  final greyStrong = const Color(0xFF272625);
+  final greyMedium = const Color(0xFF9D9995);
+  final offBlack = const Color(0xFF1E1B18);
+  final shadow4 = const Color(0xFFC5C5C5);
+  final shadow3 = const Color(0xFFABABAB);
+  final shadow2 = const Color(0xFF878787);
+  final shadow1 = const Color(0xFF696969);
+  final error = const Color(0xFFFF2D6F);
+}
