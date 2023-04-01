bce20189ce6365209b15649e8ed0db56c141d0a2
syndicate017 <bambang.trd17@gmail.com>
2023-04-01 10:53:30 +0700

    Add icon files for Android and iOS to generate Splash Screen from icon files.
    Splash Screen will be generated for Android and iOS platform
    using flutter_native_splash package with required icon files.
    
    For better implementation, it will required WidgetsFlutterBinding
    to being initialized first and then the Splash Screen can be removed when
    WidgetsFlutterBinding was finished to initialize and load all the dependencies.

--- Flutter_Weather_Sense_MVVM_BLoC/lib/main.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/main.dart
@@ -1,10 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/application.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
 
 void main() async {
   // If no binding has yet been initialized,
   // the [WidgetsFlutterBinding] class is used to create and initialize one.
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsFlutterBinding = WidgetsFlutterBinding.ensureInitialized();
 
+  // Keep native splash screen showing up until the app was finished to load the setup.
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsFlutterBinding);
+
+  // Wait the Service Locator which is using get_it package
+  // to finished its initialization setup before launch the app.
+  await initializeServiceLocator();
+
+  // Once you've decided on the Google Fonts you want in your published app,
+  // you should add the appropriate licenses to your flutter app's LicenseRegistry.
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  // Manage the Flutter Framework to keep the app to run in portrait mode only.
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  //Start running the application.
   runApp(const Application());
+
+  // Remove splash screen when the required setup is complete
+  // and the splash screen was finished to showing up.
+  FlutterNativeSplash.remove();
 }
