14b61524d848bc46ab49f438ce035c225dbc3f62
syndicate017 <bambang.trd17@gmail.com>
2023-04-18 15:21:20 +0700

    Add core functionalities for the usage failures

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/errors/failures.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class CacheFailure extends Failure {
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class InvalidInputFailure extends Failure {
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class UnexpectedFailure extends Failure {
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
