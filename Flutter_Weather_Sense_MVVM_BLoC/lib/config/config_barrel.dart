566d84f3d13b6a7ea3e0e751ea174fff85023a42
syndicate017 <bambang.trd17@gmail.com>
2023-04-18 17:39:16 +0700

    Add config barrel for exporting its dependencies

--- Flutter_Weather_Sense_MVVM_BLoC/lib/config/config_barrel.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/config/config_barrel.dart
@@ -1,3 +1,8 @@
+export 'package:extra_alignments/extra_alignments.dart';
+export 'package:flextras/flextras.dart';
+export 'package:gap/gap.dart';
+export 'package:sized_context/sized_context.dart';
+
 export 'dependency_injections/dependency_injection.dart';
 export 'routes/app_router.dart';
 export 'themes/app_colors.dart';
