319e8e0e1de24126bd1fd339bbf268f22bcc1c61
syndicate017 <bambang.trd17@gmail.com>
2023-04-27 15:32:53 +0700

    Fix utilities function of Greeting Of Day
    Add private enum GreetingType for ease of use the description
    of each GreetingType's case.
    
    Add visibleForTesting annotation above the testing method
    which only available for testing purpose only.

--- Flutter_Weather_Sense_MVVM_BLoC/lib/core/utilities/greeting_of_day/greeting_of_day.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/utilities/greeting_of_day/greeting_of_day.dart
@@ -2,26 +2,70 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
 
+/// Private [Enum] for the ease of use
+/// each description of [_GreetingType].
+enum _GreetingType {
+  morning("Good Morning"),
+  afterNoon("Good Afternoon"),
+  evening("Good Evening");
+
+  const _GreetingType(this.description);
+
+  final String description;
+}
+
 @immutable
 class GreetingOfDay {
+  /// Test the [GreetingOfDay]'s static method
+  /// [getFromTime] with the same properties.
+  /// This only available for testing purpose only.
+  @visibleForTesting
+  Either<Failure, String> testGetFromTime({
+    required DateTime time,
+  }) {
+    return GreetingOfDay.getFromTime(
+      time: time,
+    );
+  }
+
   /// Generate [String] of [TimeOfDay] status from [DateTime].
-  /// [getGreetingFromTime] will throw [UnexpectedException]
+  /// [getFromTime] will return [UnexpectedFailure]
   /// when error was occurred.
-  Either<Failure, String> getFromTime({
-    required BuildContext context,
+  static Either<Failure, String> getFromTime({
     required DateTime time,
   }) {
     try {
       final currentTime = TimeOfDay.fromDateTime(time);
-      final formattedTime = currentTime.format(context);
+      final greetingResults = _decideGreetingWithinClockRange(
+        clockTime: currentTime.hour,
+      );
 
-      if (formattedTime.isEmpty) {
+      if (greetingResults.isEmpty) {
         return Left(UnexpectedFailure());
       } else {
-        return Right(formattedTime);
+        return Right(greetingResults);
       }
     } catch (exception) {
       return Left(UnexpectedFailure());
+    }
+  }
+
+  /// Test the [GreetingOfDay]'s private static method
+  /// [_decideGreetingWithinClockRange] with the same properties.
+  /// This only available for testing purpose only.
+  @visibleForTesting
+  String testDecideGreetingWithinClockRange({required int clockTime}) {
+    return GreetingOfDay._decideGreetingWithinClockRange(clockTime: clockTime);
+  }
+
+  /// Decide the [_GreetingType]'s description from [clockTime] range.
+  static String _decideGreetingWithinClockRange({required int clockTime}) {
+    if (clockTime >= 5 && clockTime < 12) {
+      return _GreetingType.morning.description;
+    } else if (clockTime >= 12 && clockTime < 17) {
+      return _GreetingType.afterNoon.description;
+    } else {
+      return _GreetingType.evening.description;
     }
   }
 }
