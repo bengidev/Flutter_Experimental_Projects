c7e9332ba0235144eddb603581f5fc46471849f3
syndicate017 <bambang.trd17@gmail.com>
2023-04-27 15:36:49 +0700

    Fix the indentation using Google formatted style

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_screen.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_screen.dart
@@ -7,52 +7,86 @@ import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_early_w
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_greeting_description.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_location_map.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_weather_card.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            // Header Section
-            const HomeGreetingDescription(),
-            Gap($styles.insets.md),
+    final greetingDescription = useState<String>("Hallo,");
 
-            // Header Section
-            AppRawAutoCompleteField(
-              key: const ValueKey('FIRST_APP_TEXT_FORM_FIELD'),
-              objects: const <String>[
-                "Object String 1",
-                "Object String 2",
-                "Object String 3",
-              ],
-              onResult: (resultText) {
-                if (resultText == null) return;
-                debugPrint("Result Text -> $resultText");
-              },
-            ),
-            Gap($styles.insets.sm),
+    return VisibilityDetector(
+      key: const ValueKey('HOME_SCREEN_WIDGET_KEY'),
+      onVisibilityChanged: (visibilityInfo) {
+        final visiblePercentage = visibilityInfo.visibleFraction * 100;
+        if (visiblePercentage > 0) {
+          greetingDescription.value = _generateGreetingDay(
+            time: DateTime.now(),
+          );
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              // Header Section
+              HomeGreetingDescription(
+                greetingMessage: greetingDescription.value,
+              ),
+              Gap($styles.insets.md),
 
-            // Body Section
-            const HomeLocationMap(),
-            Gap($styles.insets.sm),
+              // Header Section
+              AppRawAutoCompleteField(
+                key: const ValueKey('FIRST_APP_TEXT_FORM_FIELD'),
+                objects: const <String>[
+                  "Object String 1",
+                  "Object String 2",
+                  "Object String 3",
+                ],
+                onResult: (resultText) {
+                  if (resultText == null) return;
+                  debugPrint("Result Text -> $resultText");
+                },
+              ),
+              Gap($styles.insets.sm),
 
-            // Body Section
-            const HomeWeatherCard(),
-            Gap($styles.insets.lg),
+              // Body Section
+              const HomeLocationMap(),
+              Gap($styles.insets.sm),
 
-            // Body Section
-            const HomeEarlyWarningDescription(),
-            Gap($styles.insets.sm),
+              // Body Section
+              const HomeWeatherCard(),
+              Gap($styles.insets.lg),
 
-            // Footer Section
-            const HomeEarlyWarningCards(),
-          ],
+              // Body Section
+              const HomeEarlyWarningDescription(),
+              Gap($styles.insets.sm),
+
+              // Footer Section
+              const HomeEarlyWarningCards(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  /// Generate the [String] of Greeting Day using
+  /// the static method [GreetingOfDay.getFromTime].
+  /// It will return the [String]
+  /// based on the current [DateTime].
+  String _generateGreetingDay({
+    required DateTime time,
+  }) {
+    var stringResults = "";
+    final greetingResults = GreetingOfDay.getFromTime(
+      time: time,
+    );
+    greetingResults.fold(
+      (failure) => stringResults = failure.toString(),
+      (results) => stringResults = results,
+    );
+    return stringResults;
   }
 }
