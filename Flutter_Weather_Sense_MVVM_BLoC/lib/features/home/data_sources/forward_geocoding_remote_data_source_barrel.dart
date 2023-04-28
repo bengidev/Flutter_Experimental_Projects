c450c9abea414dd2448451cc06f6d81a62b45cd3
syndicate017 <bambang.trd17@gmail.com>
2023-04-28 20:26:50 +0700

    Add barrel file of forward geocoding data source

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/data_sources/forward_geocoding_remote_data_source_barrel.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/forward_geocoding_remote_data_source_impl.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/i_forward_geocoding_remote_data_source.dart';
