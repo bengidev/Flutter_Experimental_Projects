f36754a74fdf7ca6e57ea38c4e687e26a1ad7549
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 15:39:59 +0700

    Add another core files to be exported from barrel

--- Flutter_Weather_Sense_MVVM_BLoC/lib/core/core_barrel.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/core_barrel.dart
@@ -1,6 +1,8 @@
 export 'errors/exceptions.dart';
 export 'errors/failures.dart';
 export 'utilities/greeting_of_day/greeting_of_day.dart';
+export 'utilities/network_information/network_information.dart';
+export 'utilities/shared_preferences_storage/shared_preferences_storage.dart';
 export 'widgets/app_auto_resize_text.dart';
 export 'widgets/app_button.dart';
 export 'widgets/app_circle_button.dart';
