b780effc2a60e1ffc5919aeab90b0e93fa2209fe
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 15:43:22 +0700

    Add appropriate comments to the EmptyParameter

--- Flutter_Weather_Sense_MVVM_BLoC/lib/core/use_cases/empty_parameter.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/use_cases/empty_parameter.dart
@@ -1,5 +1,8 @@
 import 'package:equatable/equatable.dart';
 
+/// The parameter object of the use case's implementation.
+/// This will give you the access of empty object
+/// with a single use case's parameter.
 class EmptyParameter extends Equatable {
   @override
   List<Object?> get props => [];
