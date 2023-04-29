4a2f1daf08ad0ac8c0f197fd3ec3b26b22f65091
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 16:41:44 +0700

    Add the appropriate comments of relevant section

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/repositories/i_forward_geocoding_repository.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/repositories/i_forward_geocoding_repository.dart
@@ -2,7 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/forward_geocoding_repository_impl.dart';
 
+/// The base class of the [ForwardGeocodingRepositoryImpl].
 @immutable
 abstract class IForwardGeocodingRepository {
   Future<Either<Failure, ForwardGeocodingModel>> searchGeocodingLocation({
