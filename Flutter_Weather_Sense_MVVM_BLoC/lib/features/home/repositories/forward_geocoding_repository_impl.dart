2795256a2a99839e1d9c0bf38f61eb594af816a4
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 16:40:22 +0700

    Fix the implementation of Forward Repository

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/repositories/forward_geocoding_repository_impl.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/repositories/forward_geocoding_repository_impl.dart
@@ -1,10 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/core/errors/failures.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/forward_geocoding_remote_data_source_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/i_forward_geocoding_repository.dart';
 
+/// The implementation of the [IForwardGeocodingRepository].
+/// This will require the implementation of all methods from
+/// [IForwardGeocodingRepository] through the overridden phase.
 @immutable
 class ForwardGeocodingRepositoryImpl implements IForwardGeocodingRepository {
   final IForwardGeocodingRemoteDataSource remoteDataSource;
@@ -14,9 +17,17 @@ class ForwardGeocodingRepositoryImpl implements IForwardGeocodingRepository {
   });
 
   @override
-  Future<Either<Failure, ForwardGeocodingModel>> searchGeocodingLocation(
-      {required String location}) {
-    // TODO: implement searchGeocodingLocation
-    throw UnimplementedError();
+  Future<Either<Failure, ForwardGeocodingModel>> searchGeocodingLocation({
+    required String location,
+  }) async {
+    try {
+      final locationResults =
+          await remoteDataSource.getSearchGeocodingLocation(location: location);
+      return Right(locationResults);
+    } on ServerException catch (serverException) {
+      return Left(ServerFailure(message: serverException.message));
+    } catch (e) {
+      return Left(UnexpectedFailure());
+    }
   }
 }
