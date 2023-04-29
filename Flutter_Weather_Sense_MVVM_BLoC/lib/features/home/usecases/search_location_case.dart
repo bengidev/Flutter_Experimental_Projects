bcc30151eb0f427ae1371b51ec0326b0d7887d7a
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 16:42:41 +0700

    Add the comments to the Search Location Case

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/usecases/search_location_case.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/usecases/search_location_case.dart
@@ -1,11 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/errors/failures.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/use_case.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/i_use_case.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/forward_geocoding_repositories_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/usecases/search_input_parameter.dart';
 
+/// The implementation of [IUseCase] with overriding
+/// the generics type of the abstract [IUseCase] itself.
+///
+/// Then it will generate the input and output based on
+/// its generics type, which will applied into the
+/// [call] callable class' method.
 @immutable
 class SearchLocationCase
     implements
