456082b43b3df7cd14a5501cd58df8915ccc03a3
syndicate017 <bambang.trd17@gmail.com>
2023-04-29 16:34:59 +0700

    Add the appropriate comments for each section

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/data_sources/i_forward_geocoding_remote_data_source.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/data_sources/i_forward_geocoding_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/forward_geocoding_remote_data_source_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model_barrel.dart';
 
+/// The base class of the [ForwardGeocodingRemoteDataSourceImpl].
 @immutable
 abstract class IForwardGeocodingRemoteDataSource {
   Future<ForwardGeocodingModel> getSearchGeocodingLocation(
