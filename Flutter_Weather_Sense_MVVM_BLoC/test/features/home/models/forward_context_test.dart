690f17d108715a3226040bd35b26a19cec34c9c3
syndicate017 <bambang.trd17@gmail.com>
2023-04-30 22:22:29 +0700

    Add unit test of Forward Context using Mocktail

---
+++ Flutter_Weather_Sense_MVVM_BLoC/test/features/home/models/forward_context_test.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_context.dart';
+
+void main() async {
+  String buildRawJsonResults() {
+    return '''
+    {
+                    "id": "region.181351",
+                    "short_code": "ID-KB",
+                    "wikidata": "Q3916",
+                    "mapbox_id": "dXJuOm1ieHBsYzpBc1Ju",
+                    "text": "West Kalimantan"
+                }
+    ''';
+  }
+
+  ForwardContext buildForwardContextFromJson() {
+    return ForwardContext.fromJson(
+      json.decode(buildRawJsonResults()) as Map<String, dynamic>,
+    );
+  }
+
+  ForwardContext buildMockForwardContext() {
+    return const ForwardContext(
+      id: "region.181351",
+      shortCode: "ID-KB",
+      wikidata: "Q3916",
+      mapboxId: "dXJuOm1ieHBsYzpBc1Ju",
+      text: "West Kalimantan",
+    );
+  }
+
+  group("Test Forward Context which is a part of Forward Geocoding Model.", () {
+    test(
+      'Given the raw data of JSON results from API, '
+      'When the raw data was converted into JSON object, '
+      'Then it should return a valid ForwardContext object .',
+      () async {
+        /// ARRANGE
+        /// ACT
+        final results = buildForwardContextFromJson();
+
+        /// ASSERT
+        expect(results, isA<ForwardContext>());
+        expect(results, equals(buildMockForwardContext()));
+      },
+    );
+
+    test(
+      'Given the raw data of JSON results from API, '
+      'When the factory constructor of ForwardContext was accessed, '
+      'Then it should return a valid object of ForwardContext subtype .',
+      () async {
+        /// ARRANGE
+        /// ACT
+        final results = ForwardContext.fromJson(
+          json.decode(buildRawJsonResults()) as Map<String, dynamic>,
+        );
+
+        /// ASSERT
+        expect(results, isA<ForwardContext>());
+      },
+    );
+
+    test(
+      'Given the raw data of JSON results from API, '
+      'When the toJson method from ForwardContext object was accessed, '
+      'Then it should return a valid JSON object.',
+      () async {
+        /// ARRANGE
+        /// ACT
+        final results = ForwardContext.fromJson(
+          json.decode(buildRawJsonResults()) as Map<String, dynamic>,
+        );
+        final resultsToJson = results.toJson();
+
+        /// ASSERT
+        expect(results, isA<ForwardContext>());
+        expect(resultsToJson, isA<Map<String, dynamic>>());
+      },
+    );
+  });
+}
