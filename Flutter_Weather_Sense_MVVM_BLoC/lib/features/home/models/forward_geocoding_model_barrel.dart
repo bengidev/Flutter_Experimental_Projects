afa43626fcec03d2612ba4cf755499a0da5afcdb
syndicate017 <bambang.trd17@gmail.com>
2023-04-30 18:34:46 +0700

    Add exported files from Forward Geocoding barrel

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/models/forward_geocoding_model_barrel.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/models/forward_geocoding_model_barrel.dart
@@ -1,1 +1,5 @@
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_context.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_feature.dart';
 export 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geometry.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_properties.dart';
