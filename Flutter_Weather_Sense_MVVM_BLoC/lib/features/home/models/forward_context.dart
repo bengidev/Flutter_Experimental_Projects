0007ceb18836e11c32173c9c35de7a3e3babeb6c
syndicate017 <bambang.trd17@gmail.com>
2023-04-30 18:32:33 +0700

    Fix safely typecast json value of Forward Context

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/models/forward_context.dart
@@ -0,0 +1,76 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
+
+/// The [ForwardContext] object will be a part of the [ForwardGeocodingModel].
+/// This object is not appropriate to be created/used for the
+/// stand-alone object.
+class ForwardContext extends Equatable {
+  final String id;
+  final String shortCode;
+  final String wikidata;
+  final String mapboxId;
+  final String text;
+
+  const ForwardContext({
+    required this.id,
+    required this.shortCode,
+    required this.wikidata,
+    required this.mapboxId,
+    required this.text,
+  });
+
+  factory ForwardContext.fromJson(Map<String, dynamic> json) {
+    // The json list value's subtype is [dynamic],
+    // so it cannot be explicitly typecast-ed to the subtype that you want.
+    // You must safely typecast the value before you used it,
+    // or it will show an error of subtype.
+
+    var defaultId = "";
+    var defaultShortCode = "";
+    var defaultWikiData = "";
+    var defaultMapboxId = "";
+    var defaultText = "";
+
+    if (json['id'] != null &&
+        json['wikidata'] != null &&
+        json['mapbox_id'] != null &&
+        json['text'] != null) {
+      final jsonId = json['id'] as String;
+      defaultId = jsonId;
+
+      final jsonShortCode = json['short_code'] as String?;
+      if (jsonShortCode != null) defaultShortCode = jsonShortCode;
+
+      final jsonWikiData = json['wikidata'] as String;
+      defaultWikiData = jsonWikiData;
+
+      final jsonMapboxId = json['mapbox_id'] as String;
+      defaultMapboxId = jsonMapboxId;
+
+      final jsonText = json['text'] as String;
+      defaultText = jsonText;
+    }
+
+    return ForwardContext(
+      id: defaultId,
+      shortCode: defaultShortCode,
+      wikidata: defaultWikiData,
+      mapboxId: defaultMapboxId,
+      text: defaultText,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'short_code': shortCode,
+        'wikidata': wikidata,
+        'mapbox_id': mapboxId,
+        'text': text,
+      };
+
+  @override
+  List<Object?> get props => [id, shortCode, wikidata, mapboxId, text];
+
+  @override
+  bool? get stringify => true;
+}
