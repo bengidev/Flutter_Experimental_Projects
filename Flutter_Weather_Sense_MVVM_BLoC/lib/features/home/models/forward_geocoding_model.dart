bff228c60c67fb771fb58036fbd432f1906a63f2
syndicate017 <bambang.trd17@gmail.com>
2023-04-30 18:33:33 +0700

    Fix safely typecast json of Forward Geocoding

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/models/forward_geocoding_model.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/models/forward_geocoding_model.dart
@@ -1,36 +1,69 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/feature.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_feature.dart';
 
 class ForwardGeocodingModel extends Equatable {
   final String type;
-  final List<String> query;
-  final List<Feature> features;
+  final List<String> queries;
+  final List<ForwardFeature> features;
   final String attribution;
 
   const ForwardGeocodingModel({
     required this.type,
-    required this.query,
+    required this.queries,
     required this.features,
     required this.attribution,
   });
 
-  factory ForwardGeocodingModel.fromJson(Map<String, dynamic> json) =>
-      ForwardGeocodingModel(
-        type: json['type'] as String,
-        query: json['query'] as List<String>,
-        features: json['features'] as List<Feature>,
-        attribution: json['attribution'] as String,
-      );
+  factory ForwardGeocodingModel.fromJson(Map<String, dynamic> json) {
+    // The json list value's subtype is [dynamic],
+    // so it cannot be explicitly typecast-ed to the subtype that you want.
+    // You must safely typecast the value before you used it,
+    // or it will show an error of subtype.
+
+    var defaultType = "";
+    var defaultQueries = <String>[];
+    var defaultFeatures = <ForwardFeature>[];
+    var defaultAttribution = "";
+
+    if (json['type'] != null &&
+        json['query'] != null &&
+        json['features'] != null &&
+        json['attribution'] != null) {
+      final jsonType = json['type'] as String;
+      defaultType = jsonType;
+
+      final jsonQueries = json['query'] as List<dynamic>;
+      final queries = jsonQueries.map<String>((e) => e as String).toList();
+      defaultQueries = queries;
+
+      final jsonFeatures = json['features'] as List<dynamic>;
+      final features = jsonFeatures
+          .map<ForwardFeature>(
+              (e) => ForwardFeature.fromJson(e as Map<String, dynamic>))
+          .toList();
+      defaultFeatures = features;
+
+      final jsonAttribution = json['attribution'] as String;
+      defaultAttribution = jsonAttribution;
+    }
+
+    return ForwardGeocodingModel(
+      type: defaultType,
+      queries: defaultQueries,
+      features: defaultFeatures,
+      attribution: defaultAttribution,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
         'type': type,
-        'query': query,
+        'query': queries,
         'features': features,
         'attribution': attribution,
       };
 
   @override
-  List<Object?> get props => [type, query, features, attribution];
+  List<Object?> get props => [type, queries, features, attribution];
 
   @override
   bool? get stringify => true;
