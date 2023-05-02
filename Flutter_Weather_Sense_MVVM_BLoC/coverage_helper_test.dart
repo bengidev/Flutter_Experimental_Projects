8ee726fc9c145ed74dfde80079f79b8444bc9237
syndicate017 <bambang.trd17@gmail.com>
2023-05-02 14:55:57 +0700

    Add coverage helper test file to import packages

---
+++ Flutter_Weather_Sense_MVVM_BLoC/coverage_helper_test.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter_weather_sense_mvvm_bloc/application.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/dependency_injections/dependency_injection.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/routes/app_router.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/themes/app_colors.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/themes/app_styles.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/errors/exceptions.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/errors/failures.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/empty_parameter.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/i_use_case.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/use_cases_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/utilities/greeting_of_day/greeting_of_day.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/utilities/network_information/network_information.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/utilities/shared_preferences_storage/shared_preferences_storage.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_auto_resize_text.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_button.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_circle_button.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_navigation_screen.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_page_indicator.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/app_raw_auto_complete_field.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/gradient_container.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/scaffold_with_navigation_bar.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/widgets/static_text_scale.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/forward_geocoding_remote_data_source_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/forward_geocoding_remote_data_source_impl.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/data_sources/i_forward_geocoding_remote_data_source.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_context.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_feature.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geometry.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_property.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/forward_geocoding_repositories_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/forward_geocoding_repository_impl.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/i_forward_geocoding_repository.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/usecases/search_input_parameter.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/usecases/search_location_case.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/usecases/search_location_case_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_early_warning_cards.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_early_warning_description.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_greeting_description.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_location_map.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_screen.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_weather_card.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_description.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_image.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_logo.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_screen.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/search_weather/views/search_weather_screen.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/generated/assets.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/main.dart';
+
+void main() {}
