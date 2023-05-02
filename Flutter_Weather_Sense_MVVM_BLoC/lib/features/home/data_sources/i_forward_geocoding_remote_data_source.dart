98b7bbc2983764380a534666b76c05dc9731a8f7
syndicate017 <bambang.trd17@gmail.com>
2023-05-02 15:03:23 +0700

    Add the appropriate comments of base remote data

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/data_sources/i_forward_geocoding_remote_data_source.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/data_sources/i_forward_geocoding_remote_data_source.dart
@@ -5,6 +5,18 @@ import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geo
 /// The base class of the [ForwardGeocodingRemoteDataSourceImpl].
 @immutable
 abstract class IForwardGeocodingRemoteDataSource {
-  Future<ForwardGeocodingModel> getSearchGeocodingLocation(
-      {required String location});
+  /// Send the [HttpClient]'s GET operation request
+  /// to the MapBox API.
+  ///
+  /// This method requires the provided [location].
+  ///
+  /// It will return the [Future] of [ForwardGeocodingModel]
+  /// when the operation results was succeeded, and it will
+  /// throw the [ServerException] when the operation results
+  /// has error which caused by Internet Connection,
+  /// or it will throw the [UnexpectedException]
+  /// when the operation results has unknown error.
+  Future<ForwardGeocodingModel> getSearchGeocodingLocation({
+    required String location,
+  });
 }
