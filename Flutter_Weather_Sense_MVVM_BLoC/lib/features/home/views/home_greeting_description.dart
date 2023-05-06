323957c3f9e90d4865b07c928654f4c613f08a75
syndicate017 <bambang.trd17@gmail.com>
2023-05-06 21:14:51 +0700

    Fix Home Greeting Description widget to Stateless

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_greeting_description.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_greeting_description.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
 
-class HomeGreetingDescription extends HookWidget {
+class HomeGreetingDescription extends StatelessWidget {
   /// Text to be placed inside header section in [HomeGreetingDescription].
   final String? greetingMessage;
 
