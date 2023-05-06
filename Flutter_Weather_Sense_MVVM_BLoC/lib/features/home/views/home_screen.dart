82b94b99387f1ae16a617cd54b31fabf87818ef9
syndicate017 <bambang.trd17@gmail.com>
2023-05-06 21:15:50 +0700

    Fix Home Screen imported files to use barrel file

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_screen.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_screen.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_early_warning_cards.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_early_warning_description.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_greeting_description.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_location_map.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_weather_card.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/views/home_views_barrel.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 class HomeScreen extends HookWidget {
