f5a31900c333c42f179bf6c36480b7e9d4917a94
syndicate017 <bambang.trd17@gmail.com>
2023-05-06 21:03:06 +0700

    Add Onboarding Views barrel file

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/onboarding/views/onboarding_views_barrel.dart
@@ -0,0 +1,8 @@
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_description.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_finish_button.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_horizontal_gradient_overlay.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_image.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_logo.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_navigation_text.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_png_extension_stack.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_svg_extension_stack.dart';
