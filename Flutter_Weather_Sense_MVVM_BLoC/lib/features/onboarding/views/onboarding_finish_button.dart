bb54438b8f19fc097d6a64b61d8f13512c5cf027
syndicate017 <bambang.trd17@gmail.com>
2023-05-06 20:55:14 +0700

    Add Onboarding Finish Button's widget

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/onboarding/views/onboarding_finish_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/config/dependency_injections/dependency_injection.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/onboarding/views/onboarding_description.dart';
+
+class OnboardingFinishButton extends StatelessWidget {
+  final BuildContext context;
+  final ValueListenable<int> valueListenable;
+  final List<OnboardingData> data;
+  final Function()? onPressed;
+
+  const OnboardingFinishButton({
+    required this.context,
+    required this.valueListenable,
+    required this.data,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: valueListenable,
+      builder: (_, pageIndex, __) {
+        return AnimatedOpacity(
+          opacity: pageIndex == data.length - 1 ? 1 : 0,
+          duration: $styles.times.fast,
+          child: AppCircleButton(
+            onPressed: onPressed ?? () {},
+            child: const Icon(Icons.arrow_forward_rounded),
+          ),
+        );
+      },
+    );
+  }
+}
