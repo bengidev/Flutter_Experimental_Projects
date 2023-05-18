4847573e2dab3535e591c7737185d1e659d23e97
syndicate017 <bambang.trd17@gmail.com>
2023-05-18 16:16:15 +0700

    Add unit test inside Shared Preferences Storage

--- Flutter_Weather_Sense_MVVM_BLoC/test/core/utilities/shared_preferences_storage/shared_preferences_storage_test.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/test/core/utilities/shared_preferences_storage/shared_preferences_storage_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/core/utilities/shared_preferences_storage/shared_preferences_storage.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model_barrel.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockSharedPreferencesStorage extends Mock
@@ -8,8 +9,28 @@ class MockSharedPreferencesStorage extends Mock
 void main() async {
   late MockSharedPreferencesStorage sharedPreferencesStorage;
 
+  ForwardFeature buildMockModelForwardFeature() {
+    return const ForwardFeature(
+      id: '',
+      type: '',
+      placeType: <String>[],
+      relevance: 0,
+      properties: ForwardProperty(wikidata: '', mapboxId: ''),
+      text: '',
+      placeName: '',
+      bbox: <double>[],
+      center: <double>[],
+      geometry: ForwardGeometry(type: '', coordinates: <double>[]),
+      context: <ForwardContext>[],
+    );
+  }
+
   setUp(() {
     sharedPreferencesStorage = MockSharedPreferencesStorage();
+  });
+
+  setUpAll(() {
+    registerFallbackValue(buildMockModelForwardFeature());
   });
 
   group("Test shared preferences storage's functionality", () {
@@ -36,7 +57,7 @@ void main() async {
         () => sharedPreferencesStorage.testSetWasOnboardingCompleted(
           wasCompleted: any(named: 'wasCompleted', that: isA<bool>()),
         ),
-      ).called(1);
+      );
     });
 
     test(
@@ -62,7 +83,7 @@ void main() async {
         () => sharedPreferencesStorage.testSetWasOnboardingCompleted(
           wasCompleted: any(named: 'wasCompleted', that: isA<bool>()),
         ),
-      ).called(1);
+      );
     });
 
     test(
@@ -78,8 +99,7 @@ void main() async {
 
       // ASSERT
       expect(results, isTrue);
-      verify(() => sharedPreferencesStorage.testGetWasOnboardingCompleted())
-          .called(1);
+      verify(() => sharedPreferencesStorage.testGetWasOnboardingCompleted());
     });
 
     test(
@@ -98,5 +118,65 @@ void main() async {
       verify(() => sharedPreferencesStorage.testGetWasOnboardingCompleted())
           .called(1);
     });
+  });
+
+  test(
+      'Given the instance of shared preferences storage, '
+      'When the value of setLatestModelForwardFeature was successfully saved, '
+      'Then it should return the Future value of boolean.', () async {
+    // ARRANGE
+    when(
+      () => sharedPreferencesStorage.testSetLatestModelForwardFeature(
+        modelFeature: buildMockModelForwardFeature(),
+      ),
+    ).thenAnswer((_) => Future<bool>.value(true));
+
+    // ACT
+    final results =
+        await sharedPreferencesStorage.testSetLatestModelForwardFeature(
+      modelFeature: buildMockModelForwardFeature(),
+    );
+
+    // ASSERT
+    expect(results, isTrue);
+    verify(
+      () => sharedPreferencesStorage.testSetLatestModelForwardFeature(
+        modelFeature: any(named: 'modelFeature', that: isA<ForwardFeature>()),
+      ),
+    );
+  });
+
+  test(
+      'Given the instance of shared preferences storage, '
+      'When the value of SetLatestModelForwardFeature was available, '
+      'Then it should return the relevant value of ForwardFeature.', () async {
+    // ARRANGE
+    when(() => sharedPreferencesStorage.testGetLatestModelForwardFeature())
+        .thenReturn(buildMockModelForwardFeature());
+
+    // ACT
+    final results = sharedPreferencesStorage.testGetLatestModelForwardFeature();
+
+    // ASSERT
+    expect(results, isA<ForwardFeature>());
+    expect(results, equals(buildMockModelForwardFeature()));
+    verify(() => sharedPreferencesStorage.testGetLatestModelForwardFeature());
+  });
+
+  test(
+      'Given the instance of shared preferences storage, '
+      'When the value of SetLatestModelForwardFeature was empty or null, '
+      'Then it should return the empty value of ForwardFeature.', () async {
+    // ARRANGE
+    when(() => sharedPreferencesStorage.testGetLatestModelForwardFeature())
+        .thenReturn(buildMockModelForwardFeature());
+
+    // ACT
+    final results = sharedPreferencesStorage.testGetLatestModelForwardFeature();
+
+    // ASSERT
+    expect(results, isA<ForwardFeature>());
+    expect(results, equals(buildMockModelForwardFeature()));
+    verify(() => sharedPreferencesStorage.testGetLatestModelForwardFeature());
   });
 }
