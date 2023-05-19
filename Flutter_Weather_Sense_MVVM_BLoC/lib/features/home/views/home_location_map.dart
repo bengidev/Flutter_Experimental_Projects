c8cbcc5e1334d2217e704a0c4c0e1bcc4b36311e
syndicate017 <bambang.trd17@gmail.com>
2023-05-19 23:08:31 +0700

    Fix MapBox functionality with change widget's type

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_location_map.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/views/home_location_map.dart
@@ -1,10 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/core_barrel.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/generated/assets.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-class HomeLocationMap extends StatelessWidget {
+class HomeLocationMap extends StatefulHookWidget {
   /// Describe the current [locationTitle]
   /// of [String] based on [TextFormField] input value.
   final String? locationTitle;
@@ -13,23 +16,59 @@ class HomeLocationMap extends StatelessWidget {
   /// of [String] based on [TextFormField] input value.
   final String? locationSubTitle;
 
-  /// Show the current location with the form
-  /// of a Map. You can change this [Widget]
-  /// into another form which are related.
-  final String? locationMap;
+  /// Coordinate location value's of [latitude] which will be
+  /// used by [MapboxMap] for building the location point.
+  final double? latitude;
+
+  /// Coordinate location value's of [longitude] which will be
+  /// used by [MapboxMap] for building the location point.
+  final double? longitude;
 
   const HomeLocationMap({
     super.key,
     this.locationTitle,
     this.locationSubTitle,
-    this.locationMap,
+    this.latitude,
+    this.longitude,
   });
+
+  @override
+  State<HomeLocationMap> createState() => _HomeLocationMapState();
+}
+
+class _HomeLocationMapState extends State<HomeLocationMap> {
+  late final MapboxMapController mapController;
+
+  @override
+  void didUpdateWidget(covariant HomeLocationMap oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    debugPrint("didUpdateWidget");
+    if (oldWidget.latitude != widget.latitude ||
+        oldWidget.longitude != widget.longitude) {
+      debugPrint("Did Update Widget was called");
+      debugPrint("Latitude -> ${widget.latitude}");
+      debugPrint("Longitude -> ${widget.longitude}");
+
+      _moveCameraPosition(
+        controller: mapController,
+        latitude: widget.latitude,
+        longitude: widget.longitude,
+      );
+
+      _addPositionMarker(
+        controller: mapController,
+        latitude: widget.latitude,
+        longitude: widget.longitude,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Expanded(
+          flex: 2,
           child: Padding(
             padding: EdgeInsets.only(
               left: $styles.insets.lg,
@@ -39,7 +78,7 @@ class HomeLocationMap extends StatelessWidget {
               children: [
                 AppAutoResizeText(
                   alignment: Alignment.centerLeft,
-                  text: locationTitle ?? "LONDON, ",
+                  text: widget.locationTitle ?? "Default Location, ",
                   textAlign: TextAlign.left,
                   textStyle: $styles.textStyle.body1Bold,
                   maxLines: 2,
@@ -47,7 +86,7 @@ class HomeLocationMap extends StatelessWidget {
                 Gap($styles.insets.xs),
                 AppAutoResizeText(
                   alignment: Alignment.centerLeft,
-                  text: locationSubTitle ?? "United Kingdom United Kingdom",
+                  text: widget.locationSubTitle ?? "Default Location",
                   textAlign: TextAlign.left,
                   textStyle: $styles.textStyle.body5Bold,
                   maxLines: 3,
@@ -57,25 +96,128 @@ class HomeLocationMap extends StatelessWidget {
           ),
         ),
         Expanded(
+          flex: 3,
           child: Padding(
             padding: EdgeInsets.only(
               left: $styles.insets.sm,
               right: $styles.insets.sm,
             ),
             child: Container(
+              padding: EdgeInsets.all($styles.insets.xs),
               height: context.heightPx * 0.15,
               decoration: BoxDecoration(
                 color: $styles.colors.tertiary,
                 borderRadius: BorderRadius.circular($styles.corners.xs),
               ),
-              child: SvgPicture.asset(
-                locationMap ?? Assets.imagesNotAvailable,
-                fit: BoxFit.cover,
+              child: MapboxMap(
+                key: widget.key,
+                accessToken: $mapBoxAccessToken,
+                styleString: $mapBoxStyleUrl,
+                myLocationEnabled: true,
+                trackCameraPosition: true,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(
+                    widget.latitude ?? -0.02269,
+                    widget.longitude ?? 109.344749,
+                  ),
+                  zoom: 12.0,
+                ),
+                dragEnabled: false,
+                zoomGesturesEnabled: false,
+                tiltGesturesEnabled: false,
+                rotateGesturesEnabled: false,
+                scrollGesturesEnabled: false,
+                compassEnabled: false,
+                onMapCreated: _onMapCreated,
+                attributionButtonMargins: const Point<double>(-100, -100),
+                logoViewMargins: const Point<double>(-100, -100),
               ),
             ),
           ),
         ),
       ],
+    );
+  }
+
+  /// Initialize the [MapboxMapController] to be able to use
+  /// the features of [MapboxMap].
+  ///
+  /// And then this will move the camera position into
+  /// the provided [LatLng] coordinates together with
+  /// the relevant position marker.
+  Future<void> _onMapCreated(MapboxMapController controller) async {
+    mapController = controller;
+
+    await _moveCameraPosition(
+      controller: mapController,
+      latitude: widget.latitude,
+      longitude: widget.longitude,
+    );
+
+    await _addPositionMarker(
+      controller: mapController,
+      latitude: widget.latitude,
+      longitude: widget.longitude,
+    );
+  }
+
+  /// Move camera position into the provided
+  /// [latitude] and [longitude] coordinates using
+  /// the [MapboxMapController] functionality.
+  Future<void> _moveCameraPosition({
+    required MapboxMapController controller,
+    required double? latitude,
+    required double? longitude,
+  }) async {
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 270.0,
+          tilt: 30.0,
+          zoom: 17.0,
+          target: LatLng(
+            latitude ?? -0.02269,
+            longitude ?? 109.344749,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Add position marker into the provided
+  /// [latitude] and [longitude] coordinates using
+  /// the [MapboxMapController] functionality.
+  ///
+  /// Before using this method, you must provide
+  /// the marker image and using that image to
+  /// create the position marker symbol.
+  Future<void> _addPositionMarker({
+    required MapboxMapController controller,
+    required double? latitude,
+    required double? longitude,
+  }) async {
+    final byteData = await rootBundle.load('assets/images/point-marker.png');
+    final pointMarkerImage = byteData.buffer.asUint8List();
+
+    await mapController.addImage('pointMarkerImage', pointMarkerImage);
+
+    await mapController.addSymbol(
+      SymbolOptions(
+        iconSize: 0.3,
+        iconImage: 'pointMarkerImage',
+        textField: widget.locationTitle ?? "Default Location",
+        textSize: 12.5,
+        textOffset: const Offset(0, 0.8),
+        textAnchor: 'top',
+        textColor: '#000000',
+        textHaloBlur: 1,
+        textHaloColor: '#ffffff',
+        textHaloWidth: 0.8,
+        geometry: LatLng(
+          latitude ?? -0.02269,
+          longitude ?? 109.344749,
+        ),
+      ),
     );
   }
 }
