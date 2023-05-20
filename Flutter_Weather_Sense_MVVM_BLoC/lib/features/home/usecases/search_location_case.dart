5c4563f7ad2b7ac29c1e5143db19d8a4b57b4bbf
syndicate017 <bambang.trd17@gmail.com>
2023-05-20 22:05:42 +0700

    Move the parameter's class into its use case

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/usecases/search_location_case.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/home/usecases/search_location_case.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/errors/failures.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/core/use_cases/i_use_case.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/models/forward_geocoding_model.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/features/home/repositories/forward_geocoding_repositories_barrel.dart';
-import 'package:flutter_weather_sense_mvvm_bloc/features/home/usecases/search_input_parameter.dart';
 
 /// The implementation of [IUseCase] with overriding
 /// the generics type of the abstract [IUseCase] itself.
@@ -31,4 +31,21 @@ class SearchLocationCase
       location: parameters.location,
     );
   }
+}
+
+/// The parameter object of the use case's implementation.
+/// This will give you the easy access of all the
+/// required object with just a single use case's parameter.
+class SearchInputParameter extends Equatable {
+  final String location;
+
+  const SearchInputParameter({
+    required this.location,
+  });
+
+  @override
+  List<Object?> get props => [location];
+
+  @override
+  bool get stringify => true;
 }
