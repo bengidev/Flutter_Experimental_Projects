d0c335a02c275fa88c27cd25b7021bcba716cfb1
syndicate017 <bambang.trd17@gmail.com>
2023-05-27 17:06:55 +0700

    Fix application route by separate functionality

--- Flutter_Weather_Sense_MVVM_BLoC/lib/config/config_barrel.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/config/config_barrel.dart
@@ -4,6 +4,7 @@ export 'package:gap/gap.dart';
 export 'package:sized_context/sized_context.dart';
 
 export 'dependency_injections/dependency_injection.dart';
-export 'routes/app_router.dart';
+export 'routes/app_go_router.dart';
+export 'routes/app_route.dart';
 export 'themes/app_colors.dart';
 export 'themes/app_styles.dart';
