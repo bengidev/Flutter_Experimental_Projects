896213b081cc2cf7af408bf51ac73c4b92aa3104
syndicate017 <bambang.trd17@gmail.com>
2023-05-27 17:54:50 +0700

    Fix including files to be available for use barrel

--- Flutter_Weather_Sense_MVVM_BLoC/lib/core/core_barrel.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/core/core_barrel.dart
@@ -5,7 +5,7 @@ export 'utilities/location_detail_helper/location_detail_helper.dart';
 export 'utilities/network_information/network_information.dart';
 export 'utilities/permissions_handle_helper/permissions_handle_helper.dart';
 export 'utilities/shared_preferences_storage/shared_preferences_storage.dart';
-export 'utilities/weather_code_into_image_helper/weather_code_into_image_helper.dart';
+export 'utilities/weather_forecast_helper/weather_forecast_helper.dart';
 export 'widgets/app_auto_resize_text.dart';
 export 'widgets/app_button.dart';
 export 'widgets/app_circle_button.dart';
