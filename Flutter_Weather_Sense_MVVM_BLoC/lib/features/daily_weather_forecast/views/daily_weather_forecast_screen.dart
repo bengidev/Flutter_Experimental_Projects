f1aa0321c7df344946bce57a7f5cb8b3572d6b9e
syndicate017 <bambang.trd17@gmail.com>
2023-05-31 14:05:53 +0700

    Fix Daily Weather Screen by separating its widget

--- Flutter_Weather_Sense_MVVM_BLoC/lib/features/daily_weather_forecast/views/daily_weather_forecast_screen.dart
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/daily_weather_forecast/views/daily_weather_forecast_screen.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_weather_sense_mvvm_bloc/config/config_barrel.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_weather_sense_mvvm_bloc/features/daily_weather_forecast/views/daily_views_barrel.dart';
 
 class DailyWeatherForecastScreen extends HookWidget {
   const DailyWeatherForecastScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final selectedDayNumber = useState<int>(0);
+    final mockDataAvailableDays = useState<List<String>>(
+      ["Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7"],
+    );
+
     return Scaffold(
       body: SafeArea(
-        child: Center(
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Search Weather Screen"),
-              ElevatedButton(
-                onPressed: () {
-                  context.pushNamed(AppGoRouter.homePath);
+              // Daily Weather Forecast Header
+              const DailyHeader(),
+              Gap($styles.insets.sm),
+
+              // Daily Weather Forecast Body
+              DailyAvailableDays(
+                availableDays: mockDataAvailableDays.value,
+                selectedDayIndex: selectedDayNumber.value,
+                onDayPressed: (index) async {
+                  debugPrint("index -> $index");
+                  selectedDayNumber.value = index;
                 },
-                child: const Text("Go To Home Screen"),
+              ),
+
+              // Daily Weather Forecast Body
+              const DailyWeatherCard(
+                weatherImage: 'assets/images/thunder_rain.svg',
+                weatherDescription: "Rainy with thunder",
+                temperatureMax: 37.5,
+                temperatureMin: 21.7,
+                windSpeed: 7.8,
+                precipitationProbability: 11.5,
+              ),
+
+              // Daily Weather Forecast Footer
+              const DailySunCard(
+                sunriseTime: "04.37",
+                sunsetTime: "17.22",
               ),
             ],
           ),
