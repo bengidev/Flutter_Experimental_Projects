a676487c1b9d3d7fc93b7d083f5427b19ceeeb32
syndicate017 <bambang.trd17@gmail.com>
2023-06-03 13:44:59 +0700

    Add barrel file for Daily Forecast repository

---
+++ Flutter_Weather_Sense_MVVM_BLoC/lib/features/daily_weather_forecast/repositories/daily_weather_forecast_repository_barrel.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_weather_sense_mvvm_bloc/features/daily_weather_forecast/repositories/daily_weather_forecast_repository/daily_weather_forecast_repository_impl.dart';
+export 'package:flutter_weather_sense_mvvm_bloc/features/daily_weather_forecast/repositories/daily_weather_forecast_repository/i_daily_weather_forecast_repository.dart';
