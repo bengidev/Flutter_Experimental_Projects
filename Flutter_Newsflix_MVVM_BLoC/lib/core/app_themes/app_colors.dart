45d028da2031eb0a93e0dfb06fd9477ad3ebb087
syndicate017 <bambang.trd17@gmail.com>
2023-07-05 15:38:34 +0700

    Add app_colors for statically using its values

---
+++ Flutter_Newsflix_MVVM_BLoC/lib/core/app_themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+final class AppColors {
+  static const primary = Color(0xFFC5832B);
+  static const secondary = Color(0xFF120E00);
+  static const accentGray = Color(0xFFF5F5F5);
+  static const customGreen = Color(0xFF38493F);
+  static const customYellowOne = Color(0xFFF7BF4F);
+  static const customYellowTwo = Color(0xFFFFCF00);
+  static const white = Color(0xFFFFFFFF);
+  static const cream = Color(0xFFFCF8E8);
+}
