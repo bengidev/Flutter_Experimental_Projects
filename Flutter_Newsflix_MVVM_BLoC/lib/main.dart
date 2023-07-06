844c4d86fc4debce5c511b41b362f1aa7a08bdbd
syndicate017 <bambang.trd17@gmail.com>
2023-07-06 14:34:49 +0700

    Fix main function for using FlutterNativeSplash

--- Flutter_Newsflix_MVVM_BLoC/lib/main.dart
+++ Flutter_Newsflix_MVVM_BLoC/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
