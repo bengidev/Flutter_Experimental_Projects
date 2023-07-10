544a036c56b7da57e1cac5528955a355a0fe2b0d
syndicate017 <bambang.trd17@gmail.com>
2023-07-10 15:20:36 +0700

    Add app_go_router for implementing GoRouter

---
+++ Flutter_Newsflix_MVVM_BLoC/lib/core/routes/app_go_router.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:newsflix/core/core_barrel.dart';
+import 'package:newsflix/features/presentation/views/intro_view.dart';
+
+class AppGoRouter {
+  static GoRouter builder = _buildRouter();
+  static const String introPath = '/intro';
+
+  /// [GlobalKey] for identify its Navigator Widget when needed.
+  static final GlobalKey<NavigatorState> _rootNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'root');
+
+  /// [GlobalKey] for identify its Shell Widget when needed.
+  static final GlobalKey<NavigatorState> _shellNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'shell');
+
+  /// Build [GoRouter] settings such as [initialLocation], [routes], [redirect], etc.
+  static GoRouter _buildRouter() {
+    final appRouter = GoRouter(
+      navigatorKey: _rootNavigatorKey,
+      debugLogDiagnostics: true,
+      initialLocation: AppGoRouter.introPath,
+      routes: [
+        AppRoute(
+          parentNavigatorKey: _rootNavigatorKey,
+          name: AppGoRouter.introPath,
+          path: AppGoRouter.introPath,
+          builder: (_, __) => const IntroView(),
+          useFade: true,
+        ),
+      ],
+    );
+
+    return appRouter;
+  }
+}
