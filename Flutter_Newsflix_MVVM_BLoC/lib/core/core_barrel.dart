f2750e97467eec19b3a8f0b9d2a896e288de84d9
syndicate017 <bambang.trd17@gmail.com>
2023-07-11 13:09:30 +0700

    Fix core_barrel for adding another export files

--- Flutter_Newsflix_MVVM_BLoC/lib/core/core_barrel.dart
+++ Flutter_Newsflix_MVVM_BLoC/lib/core/core_barrel.dart
@@ -2,3 +2,4 @@ export 'package:newsflix/core/routes/app_go_router.dart';
 export 'package:newsflix/core/routes/app_route.dart';
 export 'package:newsflix/core/themes/app_colors.dart';
 export 'package:newsflix/core/themes/app_styles.dart';
+export 'package:newsflix/core/utilities/extra_alignments.dart';
