35119c861c78622a14b3e12a02eedb08a9e44fee
syndicate017 <bambang.trd17@gmail.com>
2023-07-11 13:09:07 +0700

    Add dependency_injection to apply its concepts

---
+++ Flutter_Newsflix_MVVM_BLoC/lib/dependency_injection.dart
@@ -0,0 +1,5 @@
+import 'package:newsflix/application.dart';
+import 'package:newsflix/core/core_barrel.dart';
+
+/// Global access helpers for the ease of AppStyles' usage.
+AppStyles get $appStyles => Application.styles;
