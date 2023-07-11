bfb0f9dd1286470d5de51d90256bf7d6417665bc
syndicate017 <bambang.trd17@gmail.com>
2023-07-11 13:14:01 +0700

    Add extra_alignments for ease to use alignments

---
+++ Flutter_Newsflix_MVVM_BLoC/lib/core/utilities/extra_alignments.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/widgets.dart';
+
+final class TopLeft extends Align {
+  const TopLeft({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.topLeft);
+}
+
+final class TopCenter extends Align {
+  const TopCenter({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.topCenter);
+}
+
+final class TopRight extends Align {
+  const TopRight({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.topRight);
+}
+
+final class CenterLeft extends Align {
+  const CenterLeft({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.centerLeft);
+}
+
+final class CenterRight extends Align {
+  const CenterRight({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.centerRight);
+}
+
+final class BottomLeft extends Align {
+  const BottomLeft({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.bottomLeft);
+}
+
+final class BottomCenter extends Align {
+  const BottomCenter({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.bottomCenter);
+}
+
+final class BottomRight extends Align {
+  const BottomRight({
+    super.key,
+    super.widthFactor,
+    super.heightFactor,
+    super.child,
+  }) : super(alignment: Alignment.bottomRight);
+}
